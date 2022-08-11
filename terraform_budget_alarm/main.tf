fd4bab0a23975d3d18fcabc0b0d509a05c425511
MIN CHAN KIM <nargene@gmail.com>
2022-08-11 23:06:23 +0900

    test

--- terraform_budget_alarm/main.tf
+++ terraform_budget_alarm/main.tf
@@ -3,3 +3,11 @@ provider "aws" {
     secret_key =  "${var.secret_key}"
     region = "ap-northeast-2"
 }
+
+resource "aws_budgets_budget" "actual_cost_budget" {
+    count = var.enable_actual_cost_budget ? 1 : 0
+    name  = var.actual_cost_budget_name
+    
+     
+  
+}
